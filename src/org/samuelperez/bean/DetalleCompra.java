676ad780be30a79520370bc1d2b193b25a6f42fe
sperez-2020493 <sperez-2020493@kinal.edu.gt>
2024-05-20 15:15:09 -0600

    Detalle Compra completo

---
+++ src/org/samuelperez/bean/DetalleCompra.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.samuelperez.bean;
+
+/**
+ *
+ * @author David Perez
+ */
+public class DetalleCompra {
+    private int codigoDetalleCompra; 
+    private Double costoUnitario;
+    private int cantidad;
+    private String codigoProducto;
+    private int numeroDocumento;
+    
+    public DetalleCompra(){
+        
+    }
+
+    public DetalleCompra(int codigoDetalleCompra, Double costoUnitario, int cantidad, String codigoProducto, int numeroDocumento) {
+        this.codigoDetalleCompra = codigoDetalleCompra;
+        this.costoUnitario = costoUnitario;
+        this.cantidad = cantidad;
+        this.codigoProducto = codigoProducto;
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public int getCodigoDetalleCompra() {
+        return codigoDetalleCompra;
+    }
+
+    public void setCodigoDetalleCompra(int codigoDetalleCompra) {
+        this.codigoDetalleCompra = codigoDetalleCompra;
+    }
+
+    public Double getCostoUnitario() {
+        return costoUnitario;
+    }
+
+    public void setCostoUnitario(Double costoUnitario) {
+        this.costoUnitario = costoUnitario;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(String codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public int getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(int numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+    
+    
+}
--- src/org/samuelperez/controller/MenuDetalleComprasController.java
+++ src/org/samuelperez/controller/MenuDetalleComprasController.java
@@ -6,11 +6,28 @@
 package org.samuelperez.controller;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javax.swing.JOptionPane;
+import org.samuelperez.bean.Compras;
+import org.samuelperez.bean.DetalleCompra;
+import org.samuelperez.bean.Productos;
+import org.samuelperez.db.Conexion;
 import org.samuelperez.system.Principal;
 
 /**
@@ -18,11 +35,81 @@ import org.samuelperez.system.Principal;
  * @author David Perez
  */
 public class MenuDetalleComprasController implements Initializable {
-        private Principal escenarioPrincipal;
-        
+
+    private Principal escenarioPrincipal;
+    private ObservableList<DetalleCompra> listaDetalleCompra;
+    private ObservableList<Productos> listaProductos;
+    private ObservableList<Compras> listaCompras;
+
+    private enum operaciones {
+        AGREGAR, ELIMINAR, EDITAR, ACTUALIZAR, CANCELAR, NINGUNO
+    };
+
+    private MenuDetalleComprasController.operaciones tipoDeOperaciones = operaciones.NINGUNO;
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    private TableView tblDetalleCompras;
+
+    @FXML
+    private TableColumn colDetalleCompraD;
+
+    @FXML
+    private TableColumn colConstoUnitarioD;
+
+    @FXML
+    private TableColumn colCantidadD;
+
+    @FXML
+    private TableColumn colCodigoProductoD;
+
+    @FXML
+    private TableColumn colNumeroProductoD;
+
+    @FXML
+    private Button btnAgregar;
+
+    @FXML
+    private ImageView imgAgregar;
+
+    @FXML
+    private Button btnEditar;
+
+    @FXML
+    private ImageView imgEditar;
+
+    @FXML
+    private Button btnReport;
+
+    @FXML
+    private ImageView imgReport;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private ImageView imgEliminar;
+
+    @FXML
+    private ComboBox cmbCodigoProductos;
+
+    @FXML
+    private ComboBox cmbNumeroDocumento;
+
+    @FXML
+    private TextField txtCodigoDetalleC;
+
+    @FXML
+    private TextField txtCostoUnitario;
+
+    @FXML
+    private TextField txtCantidad;
+
     /**
      * Se encarga de devolver el escenario.
-     * 
+     *
      * @return el escenario Principal.
      */
     public Principal getEscenarioPrincipal() {
@@ -31,28 +118,327 @@ public class MenuDetalleComprasController implements Initializable {
 
     /**
      * Se encarga de establecer el ecenario Principal.
-     * 
+     *
      * @param escenarioPrincipal el escenario principal.
      */
     public void setEscenarioPrincipal(Principal escenarioPrincipal) {
         this.escenarioPrincipal = escenarioPrincipal;
     }
 
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    } 
+        cargarDatos();
+    }
 
     /**
      * Se encarga de Manejar las Acciones de los botones.
-     * 
+     *
      * @param event el evento que ocurrio en ese momento.
      */
-    @FXML Button btnRegresar;
-    public void handleButtonAction (ActionEvent event){
-        if(event.getSource() == btnRegresar){
+    public void handleButtonAction(ActionEvent event) {
+        if (event.getSource() == btnRegresar) {
             escenarioPrincipal.menuPrincipalView();
         }
-    }   
+    }
+
+    public void cargarDatos() {
+        tblDetalleCompras.setItems(getDetalleCompra());
+        colDetalleCompraD.setCellValueFactory(new PropertyValueFactory<DetalleCompra, Integer>("codigoDetalleCompra"));
+        colConstoUnitarioD.setCellValueFactory(new PropertyValueFactory<DetalleCompra, Double>("costoUnitario"));
+        colCantidadD.setCellValueFactory(new PropertyValueFactory<DetalleCompra, Integer>("cantidad"));
+        colCodigoProductoD.setCellValueFactory(new PropertyValueFactory<DetalleCompra, String>("codigoProducto"));
+        colNumeroProductoD.setCellValueFactory(new PropertyValueFactory<DetalleCompra, Integer>("numeroDocumento"));
+    }
+
+    public void seleccionarElemento() {
+        txtCodigoDetalleC.setText(String.valueOf(((DetalleCompra) tblDetalleCompras.getSelectionModel().getSelectedItem()).getCodigoDetalleCompra()));
+        txtCostoUnitario.setText(String.valueOf(((DetalleCompra) tblDetalleCompras.getSelectionModel().getSelectedItem()).getCostoUnitario()));
+        txtCantidad.setText(String.valueOf(((DetalleCompra) tblDetalleCompras.getSelectionModel().getSelectedItem()).getCantidad()));
+        cmbCodigoProductos.getSelectionModel().select(buscarProducto(((DetalleCompra) tblDetalleCompras.getSelectionModel().getSelectedItem()).getCodigoProducto()));
+        cmbNumeroDocumento.getSelectionModel().select(buscarCompra(((DetalleCompra) tblDetalleCompras.getSelectionModel().getSelectedItem()).getNumeroDocumento()));
+    }
+
+    public Productos buscarProducto(String codigoProducto) {
+        Productos resultado = null;
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("call sp_BuscarProductos(?);");
+            procedimiento.setString(1, codigoProducto);
+            ResultSet registro = procedimiento.executeQuery();
+            while (registro.next()) {
+                resultado = new Productos(registro.getString("codigoProducto"),
+                        registro.getString("descripcionProducto"),
+                        registro.getDouble("precioUnitario"),
+                        registro.getDouble("precioDocena"),
+                        registro.getDouble("precioMayor"),
+                        registro.getString("imagenProducto"),
+                        registro.getInt("existencia"),
+                        registro.getInt("codigoTipoProducto"),
+                        registro.getInt("codigoProveedor"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }
+
+    public Compras buscarCompra(int numeroDocumento) {
+        Compras resultado = null;
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("call sp_BuscarCompra(?);");
+            procedimiento.setInt(1, numeroDocumento);
+            ResultSet registro = procedimiento.executeQuery();
+            while (registro.next()) {
+                resultado = new Compras(registro.getInt("numeroDocumento"),
+                        registro.getString("fechaDocumento"),
+                        registro.getString("descripcion"),
+                        registro.getDouble("totalDocumento"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }
+
+    public ObservableList<Productos> getProductos() {
+        ArrayList<Productos> lista = new ArrayList<Productos>();
+        ResultSet resultado = null;
+
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("call sp_ListarProductos();");
+            resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new Productos(resultado.getString("codigoProducto"),
+                        resultado.getString("descripcionProducto"),
+                        resultado.getDouble("precioUnitario"),
+                        resultado.getDouble("precioDocena"),
+                        resultado.getDouble("precioMayor"),
+                        resultado.getString("imagenProducto"),
+                        resultado.getInt("existencia"),
+                        resultado.getInt("codigoTipoProducto"),
+                        resultado.getInt("codigoProveedor")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listaProductos = FXCollections.observableList(lista);
+
+    }
+
+    public ObservableList<Compras> getCompras() {
+        ArrayList<Compras> lista = new ArrayList<>();
+        ResultSet resultado = null;
+
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("call sp_ListarCompras();");
+            resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new Compras(resultado.getInt("numeroDocumento"),
+                        resultado.getString("fechaDocumento"),
+                        resultado.getString("descripcion"),
+                        resultado.getDouble("totalDocumento")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listaCompras = FXCollections.observableList(lista);
+
+    }
+
+    public ObservableList<DetalleCompra> getDetalleCompra() {
+        ArrayList<DetalleCompra> lista = new ArrayList<DetalleCompra>();
+        ResultSet resultado = null;
+
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("call sp_ListarDetalleCompra();");
+            resultado = procedimiento.executeQuery();
+            while (resultado.next()) {
+                lista.add(new DetalleCompra(resultado.getInt("codigoDetalleCompra"),
+                        resultado.getDouble("costoUnitario"),
+                        resultado.getInt("cantidad"),
+                        resultado.getString("codigoProducto"),
+                        resultado.getInt("numeroDocumento")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listaDetalleCompra = FXCollections.observableList(lista);
+
+    }
+
+    public void agregar() {
+        switch (tipoDeOperaciones) {
+            case NINGUNO:
+                activarControles();
+                btnAgregar.setText("Guardar");
+                btnEliminar.setText("Cancelar");
+                btnEditar.setDisable(true);
+                btnReport.setDisable(true);
+                imgAgregar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconGuardar.png"));
+                imgEliminar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconCancelar.png"));
+                tipoDeOperaciones = operaciones.ACTUALIZAR;
+                break;
+            case ACTUALIZAR:
+                guardar();
+                desactivarControles();
+                limpiarControles();
+                cargarDatos();
+                btnAgregar.setText("Agregar");
+                btnEliminar.setText("Eliminar");
+                btnEditar.setDisable(false);
+                btnReport.setDisable(false);
+                imgAgregar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconAgregarUsuario.png"));
+                imgEliminar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconEliminarUsuario.png"));
+                tipoDeOperaciones = operaciones.NINGUNO;
+        }
+    }
+
+    public void guardar() {
+        DetalleCompra registro = new DetalleCompra();
+        registro.setCodigoDetalleCompra(Integer.parseInt(txtCodigoDetalleC.getText()));
+        registro.setCostoUnitario(Double.parseDouble(txtCostoUnitario.getText()));
+        registro.setCantidad(Integer.parseInt(txtCantidad.getText()));
+        registro.setCodigoProducto(((Productos) cmbCodigoProductos.getSelectionModel().getSelectedItem()).getCodigoProducto());
+        registro.setNumeroDocumento(((Compras) cmbNumeroDocumento.getSelectionModel().getSelectedItem()).getNumeroDocumento());
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("call sp_AgregarDetalleCompra(?,?,?,?,?);;");
+            procedimiento.setInt(1, registro.getCodigoDetalleCompra());
+            procedimiento.setDouble(2, registro.getCostoUnitario());
+            procedimiento.setInt(3, registro.getCantidad());
+            procedimiento.setString(4, registro.getCodigoProducto());
+            procedimiento.setInt(5, registro.getNumeroDocumento());
+            procedimiento.execute();
+            listaDetalleCompra.add(registro);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminar() {
+        switch (tipoDeOperaciones) {
+            case ACTUALIZAR:
+                desactivarControles();
+                limpiarControles();
+                btnAgregar.setText("Agregar");
+                btnEliminar.setText("Eliminar");
+                btnEditar.setDisable(false);
+                btnReport.setDisable(false);
+                imgAgregar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconAgregarUsuario.png"));
+                imgEliminar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconEliminarUsuario.png"));
+                tipoDeOperaciones = operaciones.NINGUNO;
+                break;
+            default:
+                if (tblDetalleCompras.getSelectionModel().getSelectedItem() != null) {
+                    int respuesta = JOptionPane.showConfirmDialog(null, "Confirmar la Eliminacion del Registro", "Eliminar Detalle de Compra", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                    if (respuesta == JOptionPane.YES_NO_OPTION) {
+                        try {
+                            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_EliminarDetalleCompra(?)}");
+                            procedimiento.setInt(1, ((DetalleCompra) tblDetalleCompras.getSelectionModel().getSelectedItem()).getCodigoDetalleCompra());
+                            procedimiento.execute();
+                            listaDetalleCompra.remove(tblDetalleCompras.getSelectionModel().getSelectedItem());
+                            limpiarControles();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Debe seleccionar Un Proveedor para Eliminar");
+                }
+        }
+    }
+
+    public void editar() {
+        switch (tipoDeOperaciones) {
+            case NINGUNO:
+                if (tblDetalleCompras.getSelectionModel().getSelectedItem() != null) {
+                    btnEditar.setText("Actualizar");
+                    btnReport.setText("Cancelar");
+                    btnAgregar.setDisable(true);
+                    btnEliminar.setDisable(true);
+                    imgEditar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconActualizar.png"));
+                    imgReport.setImage(new Image("/org/samuelperez/images/MenuClientes_IconCancelar.png"));
+                    activarControles();
+                    txtCodigoDetalleC.setEditable(false);
+                    tipoDeOperaciones = operaciones.ACTUALIZAR;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Debe de seleccionar un cliente");
+                }
+                break;
+            case ACTUALIZAR:
+                actualizar();
+                btnEditar.setText("Editar");
+                btnReport.setText("Reporte");
+                btnAgregar.setDisable(false);
+                btnEliminar.setDisable(false);
+                imgEditar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconEditarUsuario.png"));
+                imgReport.setImage(new Image("/org/samuelperez/images/MenuClientes_IconReportUsuario.png"));
+                tipoDeOperaciones = operaciones.NINGUNO;
+                desactivarControles();
+                limpiarControles();
+                cargarDatos();
+                break;
+        }
+
+    }
+
+    public void actualizar() {
+        try {
+            PreparedStatement procedimiento = Conexion.getInstance().getConexion().prepareCall("{call sp_ActualizarDetalleCompra(?,?,?,?,?)}");
+            DetalleCompra registro = ((DetalleCompra) tblDetalleCompras.getSelectionModel().getSelectedItem());
+            registro.setCodigoDetalleCompra(Integer.parseInt(txtCodigoDetalleC.getText()));
+            registro.setCostoUnitario(Double.parseDouble(txtCostoUnitario.getText()));
+            registro.setCantidad(Integer.parseInt(txtCantidad.getText()));
+            registro.setCodigoProducto(((Productos) cmbCodigoProductos.getSelectionModel().getSelectedItem()).getCodigoProducto());
+            registro.setNumeroDocumento(((Compras) cmbNumeroDocumento.getSelectionModel().getSelectedItem()).getNumeroDocumento());
+            procedimiento.setInt(1, registro.getCodigoDetalleCompra());
+            procedimiento.setDouble(2, registro.getCostoUnitario());
+            procedimiento.setInt(3, registro.getCantidad());
+            procedimiento.setString(4, registro.getCodigoProducto());
+            procedimiento.setInt(5, registro.getNumeroDocumento());
+            procedimiento.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cancelar() {
+        desactivarControles();
+        limpiarControles();
+        btnEditar.setText("Editar");
+        btnReport.setText("Reporte");
+        btnAgregar.setDisable(false);
+        btnEliminar.setDisable(false);
+        imgEditar.setImage(new Image("/org/samuelperez/images/MenuClientes_IconEditarUsuario.png"));
+        imgReport.setImage(new Image("/org/samuelperez/images/MenuClientes_IconReportUsuario.png"));
+        tipoDeOperaciones = operaciones.NINGUNO;
+    }
+
+    public void desactivarControles() {
+        txtCodigoDetalleC.setEditable(false);
+        txtCostoUnitario.setEditable(false);
+        txtCantidad.setEditable(false);
+        cmbCodigoProductos.setDisable(true);
+        cmbNumeroDocumento.setDisable(true);
+    }
+
+    public void activarControles() {
+        txtCodigoDetalleC.setEditable(true);
+        txtCostoUnitario.setEditable(true);
+        txtCantidad.setEditable(true);
+        cmbCodigoProductos.setDisable(false);
+        cmbNumeroDocumento.setDisable(false);
+    }
+
+    public void limpiarControles() {
+        txtCodigoDetalleC.clear();
+        txtCostoUnitario.clear();
+        txtCantidad.clear();
+        cmbCodigoProductos.getSelectionModel().getSelectedItem();
+        cmbNumeroDocumento.getSelectionModel().getSelectedItem();
+    }
 }
